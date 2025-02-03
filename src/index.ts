c68d002bed329bc1414ddb5f5749da59184d3d2e
Lukassolna <lukassolna@live.se>
2025-02-03 16:43:30 +0100

    Created initial files

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send(' Example Express + TypeScript Server');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Hello World');
+   });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
