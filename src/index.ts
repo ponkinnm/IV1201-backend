9b56aacc9d19a617de94a9191b4851d3e624c8ff
Lukassolna <lukassolna@live.se>
2025-02-03 18:02:53 +0100

    testing basic database query

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import pool from './db';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,12 +7,23 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send(' Example Express + TypeScript Server');
+  res.send('Example Express + TypeScript Server');
 });
 
-app.get('/test', (req, res) => {
-    res.send('Hello World');
-   });
+app.get('/hello', (req, res) => {
+  res.send('Hello World');
+});
+
+// 
+app.get('/testing', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM public.person');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
