a4625d6d4bbd72c8bf4089a5c227be62289b5a13
anmatar <ahmadmatar8@gmail.com>
2025-02-05 15:02:00 +0100

    deploy databse

--- src/db.ts
+++ src/db.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Heroku Postgres
+  },
 });
+
 
 export default pool;
