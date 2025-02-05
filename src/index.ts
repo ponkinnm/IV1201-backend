345b4db9584fbe31a674fdcf39481044227eb302
Lukassolna <lukassolna@live.se>
2025-02-05 15:17:39 +0100

    restructure files

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,11 @@
 import express from 'express';
-import pool from './db';
+import router from './routes/routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Example Express + TypeScript Server');
-});
-
-app.get('/hello', (req, res) => {
-  res.send('Hello World');
-});
-
-// 
-app.get('/testing', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM public.person');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
