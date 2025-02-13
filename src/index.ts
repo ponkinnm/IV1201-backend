221777e343a49d0505e010e6888dab7dfd222605
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2025-02-13 20:10:26 +0100

    Add auth layers and /login endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import sequelize from './config/dbsetup';
 import cors from 'cors'; 
 import router from './routes/routes';
+import { authRouter } from "./routes/auth";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors()); 
 app.use(express.json());
+app.use(cookieParser());
+app.use('/auth', authRouter);
 app.use('/', router);
 
 app.listen(port, async () => {
