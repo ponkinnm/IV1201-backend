5dc95317d5ffa26347b49ac8156e70978eeb3323
Befi99 <benfil@kth.se>
2025-02-13 15:21:50 +0100

    Configured cors

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ import router from './routes/routes';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); 
+//Only allow requests coming from the frontend domain.
+const corsOptions = {origin : 'https://iv1201-frontend.vercel.app'};
+
+app.use(cors(corsOptions)); 
 app.use(express.json());
 app.use('/', router);
 
