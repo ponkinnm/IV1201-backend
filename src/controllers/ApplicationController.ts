ef40f44a032e4d81aafee32c17d89d71fb74583e
Lukassolna <lukassolna@live.se>
2025-02-23 19:27:13 +0100

    check user.role_id in service layer

--- src/controllers/ApplicationController.ts
+++ src/controllers/ApplicationController.ts
@@ -16,42 +16,43 @@ export class ApplicationController {
     getAllApplications: RequestHandler = async (req, res, next) => {
         try {
           console.log('token:', req.cookies.accessToken);
-        console.log('decoded user:', req.user);
-        // we should be able to check things with eg req.user.role_id); 
-        //however we need to add this to the jwt token
+          console.log('decoded user:', req.user);
+          // we should be able to check things with eg req.user.role_id); 
+          //however we need to add this to the jwt token
           const applications = await sequelize.transaction(async () => {
-            return await this.applicationService.getAllApplications();
+            return await this.applicationService.getAllApplications(req.user!);
           });
     
           res.json(applications);
         } catch (err) {
           next(err); // Automatically rolls back on error
         }
-      };
+    };
 
 
-      getApplicationDetailsById : RequestHandler = async (_req, res , next) => {
+    getApplicationDetailsById : RequestHandler = async (_req, res , next) => {
         try{
           const application_id = parseInt(_req.params.application_id, 10);
           const applicationDetail = await sequelize.transaction(async () =>{
-              return await this.applicationService.getApplicationDetailsById(application_id);
+              return await this.applicationService.getApplicationDetailsById(application_id, _req.user!);
           });
 
           if (!applicationDetail) {
             res.status(404).json({ error: 'Application not found' });
-          }else
+          }else{
               res.json(applicationDetail);
+          }
         } catch(err){
           next(err); // Automatically rolls back on error
         }
-      };
-      /**
-       * 
-       * @param _req request from user, contain the application_id and the new status id
-       * @param res  response with the updated row
-       * @param next 
-       */
-      updateApplicationStatus: RequestHandler = async (_req, res, next) => {
+    };
+    /**
+     * 
+     * @param _req request from user, contain the application_id and the new status id
+     * @param res  response with the updated row
+     * @param next 
+     */
+    updateApplicationStatus: RequestHandler = async (_req, res, next) => {
         try{
           const application_id = parseInt(_req.params.application_id, 10);
 
@@ -59,7 +60,7 @@ export class ApplicationController {
           const { new_status_id } = _req.body;
 
           const updatedRow = await sequelize.transaction( async () => {
-            return await this.applicationService.updateApplicationStatus(application_id, new_status_id);
+            return await this.applicationService.updateApplicationStatus(application_id, new_status_id, _req.user!);
           });
           if(!updatedRow){
             res.status(404).json({error: ' filed to update status '});
@@ -68,5 +69,5 @@ export class ApplicationController {
         }catch(err){
           next(err);
         }
-      }
+    }
 }
--- src/services/ApplicationService.ts
+++ src/services/ApplicationService.ts
@@ -1,36 +1,52 @@
 import { IApplicationRepository } from "../repositories/contracts/IApplicationRepository";
+import { PersonDTO } from "../models/PersonDTO";
 
 export class ApplicationService {
-    private applicationRepository: IApplicationRepository;
+   private applicationRepository: IApplicationRepository;
 
-    constructor(applicationRepository: IApplicationRepository) {
-        this.applicationRepository = applicationRepository;
-    }
+   constructor(applicationRepository: IApplicationRepository) {
+       this.applicationRepository = applicationRepository;
+   }
 
-    async getAllApplications() {
-        try {
-            return await this.applicationRepository.getAllApplications();
-        } catch (err) {
-            console.error(err);
-            throw new Error('Failed to get applications');
-        }
-    }
+   async getAllApplications(user: PersonDTO) {
+       console.log(user.role_id);
+       if (user.role_id !== 1) {
+           throw new Error('Unauthorized. Only recruiters can access this.');
+       }
 
-    async getApplicationDetailsById(application_id : number){
-        try{
-            return await this.applicationRepository.getApplicationDetailsById(application_id);
-        } catch(err){
-            console.error(err);
-            throw new Error('Failed to get application details');
-        }
-    }
+       try {
+           return await this.applicationRepository.getAllApplications();
+       } catch (err) {
+           console.error(err);
+           throw new Error('Failed to get applications');
+       }
+   }
 
-    async updateApplicationStatus(application_id:number, new_status_id : number){
-        try{
-            return await this.applicationRepository.updateApplicationStatus(application_id,  new_status_id);
-        } catch(err){
-            console.error(err);
-            throw new Error('Failed to update application');
-        }
-    }
+   async getApplicationDetailsById(application_id: number, user: PersonDTO) {
+       console.log(user.role_id);
+       if (user.role_id !== 1) {
+           throw new Error('Unauthorized. Only recruiters can access this.');
+       }
+
+       try{
+           return await this.applicationRepository.getApplicationDetailsById(application_id);
+       } catch(err){
+           console.error(err);
+           throw new Error('Failed to get application details');
+       }
+   }
+
+   async updateApplicationStatus(application_id: number, new_status_id: number, user: PersonDTO) {
+       console.log(user.role_id); 
+       if (user.role_id !== 1) {
+           throw new Error('Unauthorized. Only recruiters can access this.');
+       }
+
+       try {
+           return await this.applicationRepository.updateApplicationStatus(application_id, new_status_id);
+       } catch(err){
+           console.error(err);
+           throw new Error('Failed to update application');
+       }
+   }
 }
