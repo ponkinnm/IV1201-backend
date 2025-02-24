11388e76453125e4ecfe548c5fdc7e97e495e32c
AhmadNmatar <117222424+AhmadNmatar@users.noreply.github.com>
2025-02-24 13:50:56 +0100

    Update IPersonRepository.ts

--- src/repositories/contracts/IPersonRepository.ts
+++ src/repositories/contracts/IPersonRepository.ts
@@ -3,11 +3,11 @@ import { PersonDTO } from "../../models/PersonDTO";
 
 
 export interface IPersonRepository {
-  findUserByUsername(username: string): Promise<Person | null>;
-  findUserByEmail(email: string): Promise<Person | null>;
+  findUserByUsername(username: string): Promise<PersonDTO | null>;
   getUserDetailById(person_id : number): Promise<PersonDTO | null>;
 
   findUserByEmail(email: string): Promise<PersonDTO | null>;
+  
   addNewUser(name: string, surname:string, pnr: number, email: string, username:string, password: string, role_id :number) : Promise<PersonDTO>;
   addNewPassword(person_id: number, new_password: string) : Promise<Person | null>;
   addNewEmail(person_id :number, new_email: string): Promise<Person | null>;
