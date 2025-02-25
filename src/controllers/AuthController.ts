1be3b4af6239a3f818e3837ad29e7e5fc7bf6d74
Pontus Kinnmark <82177239+ponkinnm@users.noreply.github.com>
2025-02-25 22:08:40 +0100

    (12) Catch errors on login route

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -8,13 +8,14 @@ export class AuthController {
     this.authService = authService;
   }
 
-  login: RequestHandler = async (req, res) => {
-    const { username, password } = req.body;
-    if (!username || !password) {
-      res.status(400).send('Missing username or password');
-      return;
-    }
-    const user = await this.authService.findUserAndVerifyPassword(username, password);
+  login: RequestHandler = async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      if (!username || !password) {
+        res.status(400).send('Missing username or password');
+        return;
+      }
+      const user = await this.authService.findUserAndVerifyPassword(username, password);
 
     if (!user) {
       res.status(401).send('Invalid username or password');
@@ -23,11 +24,14 @@ export class AuthController {
 
     const accessToken = this.authService.generateJwtToken(user);
 
-    res.cookie(this.authService.JWT_COOKIE_NAME, accessToken, { 
-      httpOnly: true, 
-      secure: true,
-      sameSite: 'none' 
-    });
-    res.json({ username: user.username, name: user.name, id: user.person_id })
+      res.cookie(this.authService.JWT_COOKIE_NAME, accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      });
+      res.json({ username: user.username, name: user.name, id: user.person_id })
+    } catch (err) {
+      next(err);
+    }
   }
 }
