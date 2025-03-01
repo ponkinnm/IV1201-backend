bd307b4812e729ec7a595103b1567af66c8ae746
AhmadNmatar <117222424+AhmadNmatar@users.noreply.github.com>
2025-03-01 12:22:20 +0100

    Update AuthController.ts

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -113,7 +113,7 @@ export class AuthController {
             const updatedUser = await sequelize.transaction(async ()=>{
                 return await this.authService.addNewPassword(person_id, password);
             });
-            res.status(200).json(updatedUser);
+            res.status(200).senc({"password has been updated"});
         } catch(err){
             next(err)
         }
